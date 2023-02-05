f67f9249fb76c640f5c97f1234f9feb5220e4f96
FabienC <fabiencoutant.ctt@gmail.com>
2023-02-05 15:36:13 +0200

    feat: init hardhat project

---
+++ hardhat.config.ts
@@ -0,0 +1,37 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-preprocessor";
+import { HardhatUserConfig } from "hardhat/config";
+import { getRemappings } from "./utils/gitRemappings";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./src", // Use ./src rather than ./contracts as Hardhat expects
+    cache: "./cache_hardhat", // Use a different cache for Hardhat than Foundry
+  },
+  // This fully resolves paths for imports in the ./lib directory for Hardhat
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          getRemappings().forEach(([find, replace]) => {
+            if (line.match(find)) {
+              line = line.replace(find, replace);
+            }
+          });
+        }
+        return line;
+      },
+    }),
+  },
+};
+
+export default config;
